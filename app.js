4737f869b1da65085b1dc0461f628156841b4678
Angel Miranda <angelmirandazambrana@gmail.com>
2017-06-11 10:53:56 +0200

    First route added

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express=require('express');
 var app=express();
+var routes=require('./routes');
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('*', function(req, res){
-    res.send('Express response');
-});
+app.get('/', routes.index);
 
 app.listen(app.get('port'));
-console.log("App server running on port "+app.get('port'));
+console.log("App server running on port " + app.get('port'));
