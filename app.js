369989c5f309384418299a42320788a3d36dbd05
Angel Miranda <angelmirandazambrana@gmail.com>
2017-06-23 08:23:52 +0200

    exercise 1 done

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 var express=require('express');
 var app=express();
+var http=require('http');
 var routes=require('./routes');
 var errorHandlers=require('./middleware/errorhandlers');
 var ejs=require('ejs');
 var partials=require('express-partials');
- 
+var bodyParser=require('body-parser');
+var reload=require('reload');
+var server=http.createServer(app);
+
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'EJS');
 app.set('views', './views');
 app.use(express.static(__dirname+'/static'));
 app.use(partials());
 app.set('view options',{defaulLayout:'layout'});
+app.use(bodyParser.urlencoded({ extended: false }))
+
+reload(server, app);
 
 app.get('/', routes.index);
-app.get('/E1', routes.e1);
+app.get('/e1', routes.e1);
+app.get('/e2', routes.e2);
+app.post('/processDataE1', routes.processDataE1);
 app.use(errorHandlers.notFound);
 
-app.listen(app.get('port'));
-console.log("App server running on port " + app.get('port'));
+
+
+
+
+server.listen(app.get('port'), function(){
+    console.log("App server running on port " + app.get('port'));
+});
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,48 @@
+const fs=require('fs');
 module.exports.index=index;
 module.exports.e1=e1;
+module.exports.e2=e2;
+module.exports.processDataE1=processDataE1;
 
 function index(req, res) {
     res.render('index', {'title':'Index'});
 };
 
 function e1(req, res){
-    res.render('e1', {'title':'Exercise 1'});
+    fs.readFile('./data/e1data.json', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        var userData=JSON.parse(data);
+    res.render('e1', {
+        'title':'Exercise 1',
+        'data': userData
+    });
+});
+};
+
+function e2 (req, res){
+    res.render('e2', {
+        'title':'Exercise 2'
+    });
+};
+
+function processDataE1(req, res){
+    fs.readFile('./data/e1data.json', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        var userData=JSON.parse(data);
+        userData.push({
+            'firstName': req.body.firstName,
+            'age': req.body.age,
+            'userName': req.body.userName
+        });
+        fs.writeFile('./data/e1data.json', JSON.stringify(userData), function (err) {
+            if (err) {
+                throw err;
+            }
+        })
+        res.redirect('/E1');
+    })
 }
